ae1157304dc4a8c79c4bda3b1fa76d336b916ca4
Gkung <getjung16@gmail.com>
2025-01-28 19:18:25 +0700

    Update Cors

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import { PeerServer } from "peer";
+import express from "express";
 
-const peerServer = PeerServer({
-  port: 9000,
-  path: "/myapp",
+const app = express();
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
+
+const peerServer = PeerServer({ port: 9000, path: "/myapp" });
+app.use(peerServer);
 
 console.log("PeerJS server running on port 9000");
